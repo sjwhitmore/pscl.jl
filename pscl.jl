ac988327898766b3d602a2f3f5ba8ea17ed53a0a
sjwhitmore <whitmore.samantha@gmail.com>
2014-09-04 18:09:23 -0400

    mcmc x0 initialization, pscl.jl acting as main for now

--- pscl.jl
+++ pscl.jl
@@ -4,3 +4,10 @@ include("apikeys.jl")
 load_sunlight_key()
 
 include("roll_call_builder.jl")
+include("mcmc.jl")
+
+rollcall=build_roll_call(sunlight_key, "al", "lower", "2011-2014")
+x0=initialize_x0(rollcall)
+print(rollcall)
+print("This is x0:")
+print(x0)
