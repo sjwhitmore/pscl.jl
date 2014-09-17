4fa8210b4c043e3e604e7e4386258fbb68e2e206
sjwhitmore <whitmore.samantha@gmail.com>
2014-09-17 18:52:55 -0400

    beta and alpha initialization method writen, update variable method begun in mcmc.jl

--- mcmc.jl
+++ mcmc.jl
@@ -1,4 +1,5 @@
 using DataFrames
+using GLM
 
 # function initialize_mcmc(rollcall,colnames)
 # 	meanrollcall=copy(rollcall)
@@ -19,6 +20,7 @@ using DataFrames
 # end
 
 function initialize_x0(rollcall)
+	# subtract out column and row means, as instructed
 	meanrollcall=copy(rollcall)
 	for j in 1:size(meanrollcall,2)
 		meanrollcall[:,j] = meanrollcall[:,j] - mean(meanrollcall[:,j])
@@ -27,13 +29,47 @@ function initialize_x0(rollcall)
 		meanrollcall[i,:] = meanrollcall[i,:] - mean(meanrollcall[i,:])
 	end
 	trans = transpose(meanrollcall)
-	corr = trans*meanrollcall
+
+	# get nxn correlation matrix so that we can find eigenvalues for initial x0
+	corr = meanrollcall*trans
 	eigs = eig(corr)
-	x0 = eigs[2][1]
+	max_eig_loc = findmax(eigs[1])[2]
+
+	x0 = eigs[2][:,max_eig_loc]
 
 	x0
 end
 
-function obtain_start_beta(rollcall,init_x)
+function initialize_beta(rollcall,init_x)
 	#implement probit regression model
+
+	# for each bill (j = 1:m)
+	# perform regression using GLM on the pertaining col/ of the roll call matrix
+	# and all the x0s
+	# this will give one beta (x coeff) and one alpha (intercept) (= B_j and a_j)
+	beta0 = Array(Float64, size(rollcall,2))
+	alpha0 = Array(Float64, size(rollcall,2))
+	for j in 1:size(rollcall,2)
+		data = DataFrame(X=init_x,Y=rollcall[:,j])
+		result = glm(Y~X,data,Binomial(),ProbitLink())
+		beta0[j] = coef(result)[2]
+		alpha0[j] = coef(result)[1]
+	end
+
+	#return initial beta and alpha
+	(beta0, alpha0)
 end
+
+
+# function for iterative MCMC process
+function update_vars(rollcall, x, beta, alpha)
+
+	# first sample y* from Normal distribution N(x(t-1)beta(t-1) - alpha(t-1)),1)
+	# truncated based on observed 1 or 0 below or above 0
+	# for abstentions we sample y* from untruncated mu(t-1)
+
+	# next sample beta
+
+	(x, beta, alpha)
+end
+
